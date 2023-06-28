b6ce68f8dc15569d2ea40e8a0531c9222e8796c9
Kushagra Chandra <kushagra.c@kitlabs.in>
2023-06-29 00:21:40 +0530

    Initial Commit

---
+++ RadiusAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RadiusAssignment
+//
+//  Created by Kushagra Chandra on 29/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("API : https://my-json-server.typicode.com/iranjith4/ad-assignment/db")
+    }
+
+
+}
+
